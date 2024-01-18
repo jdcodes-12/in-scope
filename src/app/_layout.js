9a935ad5c8839a155c3716eacb5d8469deadc943
Johnny Dickson <johnnydicksoncode@gmail.com>
2024-01-17 23:51:07 -0600

    ADD(ui/view): Home Screen - Style Welcome Section
    Finish Minimal UI Layout
    
    Finish minimal UI work for the welcome section on the
    home screen.

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,5 +1,30 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Prevent splash screen from being hidden.
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-  return <Stack />;
+  const [fontsInAssetBundle] = useFonts({
+    DMBold: require ('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require ('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require ('../assets/fonts/DMSans-Regular.ttf'),
+  }); 
+
+  // Hides the native splash screen once the fonts, only
+  // if the fonts have been loaded. Once the fonts are loaded,
+  // the splash screen is then hidden.
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsInAssetBundle) 
+      await SplashScreen.hideAsync();
+
+  }, [fontsInAssetBundle]);
+
+  return (
+    fontsInAssetBundle ? 
+      <Stack onLayoutRootView={onLayoutRootView} /> : 
+      null  
+  );
 }
--- src/ui/views/home/welcome/Welcome.jsx
+++ src/ui/views/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './welcome.style'
 import { useRouter } from 'expo-router';
 import { default as icons } from '../../../constants/icons';
@@ -12,10 +12,82 @@ import {
   FlatList
 } from 'react-native'
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 export default function Welcome() {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState('Full-time');
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <ViewHeader />
+      <SearchBar />
+      <JobTabs 
+        activeTab={activeTab} 
+        setActiveTab={setActiveTab}
+      />
     </View>
-  )
+  );
+}
+
+function ViewHeader() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.userName}>Hello Johnny,</Text>
+      <Text style={styles.welcomeMessage}>Find your perfect job, today.</Text>
+    </View>
+  );
+}
+
+function SearchBar() {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        <TextInput 
+          style={styles.searchInput}
+          value=''
+          onChange={() => {}}
+          placeholder='What job are you looking for?'
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.searchBtn}
+        onPress={() => {}}
+      >
+        <Image 
+          source={icons.search}
+          resizeMode='contain'
+          style={styles.searchBtnImage}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function JobTabs({ activeTab, setActiveTab }) {
+  return (
+    <View style={styles.tabsContainer}>
+      <FlatList
+        horizontal
+        data={jobTypes}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.tab(activeTab, item)}
+            // Changing active tab state, 
+            // updating view Stack (i.e. changing routes).
+            onPress={() => {
+              setActiveTab(item);
+              router.push(`/search/${item}`)
+            }}
+          >
+            <Text style={styles.tabText(activeTab, item)}>
+              {item}
+            </Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle={{ columnGap: SIZES.small }}
+      />
+    </View>
+  );
 }
